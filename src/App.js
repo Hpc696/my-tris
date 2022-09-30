d47a845352b6600a681e4b2c664e06eb3d3a1f62
Matteo_G <honropgiochi@gmail.com>
2022-09-30 10:28:48 +0200

    inizio

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
+import logo from './tic-tac-toe-game-svgrepo-com.svg';
 import './App.css';
+import './Gioca.js';
 
 function Bottone(){
+  Giocare();
   return(
-    <button>Ciao</button>
+    <button>GIOCA</button>
+    
+    //esegui js gioco?
   )
 }
+
+function Giocare(){
+  
+}
+//implementare poi nuove funzionalità: 'nuova partita', 'punteggio salvato'  ...  poi vs pc con difficoltà selezonabile ?-? 
 function App() {
-  const pippo= Bottone();
+  const gioca= Bottone();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {pippo}
+        <img src={logo} className="App-logo" alt="logo" width={100} height={100} />
+        {gioca}
       </header>
     </div>
   );
